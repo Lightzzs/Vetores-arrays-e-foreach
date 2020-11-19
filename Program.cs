1105c767b9526fb3ec6179ee6e08af4ddd3017d3
VINICIUS <marvinicius78910@gmail.com>
2020-11-19 20:43:16 -0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VETORES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] produtos = {"maçã", "batata", "cenoura"};
+            double[] precos = {1.1, 2.3, 4.5};
+            int[] idades = {16, 17, 17};
+
+            int tamanho = 15;
+
+            string[] nomesalunos = new string[tamanho];
+
+            //Console.WriteLine(produtos[1]);
+
+            //Console.WriteLine("Trocar batata por qual item?");
+
+            //produtos[1] = Console.ReadLine();
+
+            //Console.WriteLine(produtos[1]);
+
+            for (int i = 0; i <precos.Length; i++)
+        {
+            Console.WriteLine(i);
+            Console.WriteLine(precos[i]);
+        }
+
+        }
+    }
+}
